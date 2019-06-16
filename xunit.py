3aa7fae965990b958022eb83ff2f4b5ae61f462c
ShunsukeKouno111 <kouno.shunsuke@isid.co.jp>
2019-06-16 20:38:04 +0900

    update xunit.py

--- xunit.py
+++ xunit.py
@@ -1,5 +1,21 @@
+class TestCase:
+    def __init__(self,name):
+        self.name = name
+    def run(self):
+        method = getattr(self,self.name)
+        method()
+
+class WasRun(TestCase):
+    def __init__(self,name):
+        self.wasRun = False
+        super().__init__(name)
+    def testMethod(self):
+        self.wasRun = True
+
+
+
 test = WasRun("testMethod")
-print(test.wasRun)
-test.testMethod()
-print(test.wasRun)
+print('Did the test run?:' + str(test.wasRun))
+test.run()
+print('Did the test run?:' + str(test.wasRun))
 
