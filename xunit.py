90731abb67afce05e8cf0ae2a7c5b64b5f23540c
ShunsukeKouno111 <kouno.shunsuke@isid.co.jp>
2019-06-23 17:50:02 +0900

    update xunit.py 21章

--- xunit.py
+++ xunit.py
@@ -1,23 +1,47 @@
 class TestCase:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
+
     def setUp(self):
         pass
+
     def tearDown(self):
         pass
+
     def run(self):
+        result = testResult()
+        result.testStarted()
         self.setUp()
-        method = getattr(self,self.name)
+        method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
+
 
 class WasRun(TestCase):
     def setUp(self):
         self.log = "setUp "
+
     def testMethod(self):
         self.log = self.log + "testMethod "
+
+    def testBrokenMethod(self):
+        raise Exception
+
     def tearDown(self):
-        self.log = self.log + "tearDown "        
+        self.log = self.log + "tearDown "
+
+
+class testResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
@@ -25,4 +49,17 @@ class TestCaseTest(TestCase):
         test.run()
         assert("setUp testMethod tearDown " == test.log)
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+
+
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+# TestCaseTest("testFailedResult").run()
